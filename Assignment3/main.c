bb2ed6e9613d5d9248510345c7902707419194a6
mbertolino <mattias.bertolino@gmail.com>
2017-02-07 11:44:30 +0100

    Added library stdlib

--- Assignment3/main.c
+++ Assignment3/main.c
@@ -1,10 +1,11 @@
 #include "file_operations.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 	int a;
 	int nParticles = 2;
-	double* buffer = malloc(nParticles*sizeof(double));
+	double* buffer = (double*)malloc(nParticles*sizeof(double));
 	a = read_doubles_from_file(nParticles, buffer, "filename");
 	
 	return 0;
