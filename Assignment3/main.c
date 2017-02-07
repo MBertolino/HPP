054a364aa1a897d144d07e63a73966a4818ceb6a
aaaaarvi <arvi.jj@gmail.com>
2017-02-07 12:23:14 +0100

    Changed main, added makefile

--- Assignment3/main.c
+++ Assignment3/main.c
@@ -1,12 +1,20 @@
-#include "file_operations.h"
+#include "file_operations/file_operations.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
 	int a;
-	int nParticles = 2;
-	double* buffer = (double*)malloc(nParticles*sizeof(double));
-	a = read_doubles_from_file(nParticles, buffer, "filename");
-	
+	int nLines = 10;
+	double* buffer = (double*)malloc(nLines*sizeof(double));
+	a = read_doubles_from_file(nLines, buffer, "input_data/circles_N_2.gal");
+  
+  int i;
+  for (i = 0; i < nLines; i++) {
+    printf("%lf\n", buffer[i]);
+  }
+  
+	printf("\na = %i", a);
+  
+  free(buffer);
 	return 0;
 }
