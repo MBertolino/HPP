840dc8f08d0b15a8a6b854869ed18a7b54a98327
mbertolino <mattias.bertolino@gmail.com>
2017-02-07 11:31:33 +0100

    test main

--- Assignment3/main.c
+++ Assignment3/main.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 
 int main() {
-
-
+	int a;
+	int nParticles = 2;
+	double* buffer = malloc(nParticles*sizeof(double));
+	a = read_doubles_from_file(nParticles, buffer, "filename");
+	
+	return 0;
 }
