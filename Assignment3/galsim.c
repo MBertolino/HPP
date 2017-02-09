9d0b678bb03a04143bdca83480900ffb28530a3a
mbertolino <mattias.bertolino@gmail.com>
2017-02-09 17:18:10 +0100

    fix

--- Assignment3/galsim.c
+++ Assignment3/galsim.c
@@ -27,8 +27,10 @@ int main(int argc, char *argv[]) {
   const int graphics = atoi(argv[5]);
   
   /* Setup graphics */
-  if (graphics)
+  if (graphics) {
     InitializeGraphics(argv[5], windowWidth, windowWidth);
+    SetCAxes(0,1);
+  }
   float L = 1;
   float W = 1;
   float radius = 0.01*L;
@@ -47,42 +49,40 @@ int main(int argc, char *argv[]) {
   printf("Reading input file: flag = %i\n", flag);
   
   /* Creating the particles */
-  particle_t **particles  = (particle_t**)malloc(sizeof(particle_t*));
-  //particle_t (*particles)[N];
+  particle_t **particles = (particle_t**)malloc(N*sizeof(particle_t*));
+  //particle_t *particles[N];
   for (short i = 0; i < N; i++) {
     particles[i] = (particle_t*)malloc(sizeof(particle_t));
-    particles[i]->x = data[5*(i)];
-    particles[i]->y = data[5*(i) + 1];
-    particles[i]->m = data[5*(i) + 2];
-    particles[i]->vx = data[5*(i) + 3];
-    particles[i]->vy = data[5*(i) + 4];
+    particles[i]->x = data[5*i];
+    particles[i]->y = data[5*i + 1];
+    particles[i]->m = data[5*i + 2];
+    particles[i]->vx = data[5*i + 3];
+    particles[i]->vy = data[5*i + 4];
   }
   
   /* Create an array for the previous particle states */
-  particle_t **particlesPrev = (particle_t**)malloc(sizeof(particle_t*));
-  //particle_t (*particlesPrev)[N];
+  particle_t **particlesPrev = (particle_t**)malloc(N*sizeof(particle_t*));
+  //particle_t *particlesPrev[N];
   for (short i = 0; i < N; i++) {
     particlesPrev[i] = (particle_t*)malloc(sizeof(particle_t));
   }
   
   /* Loop over time */
   for (short k = 0; k < nsteps; k++) {
+    printf("%d\n", k);
      
     /* Update previous particles */
     for (short j = 0; j < N; j++) {
-    particlesPrev[j] = particles[j];
-      /*
       particlesPrev[j]->x = particles[j]->x;
       particlesPrev[j]->y = particles[j]->y;
       particlesPrev[j]->m = particles[j]->m;
       particlesPrev[j]->vx = particles[j]->vx;
       particlesPrev[j]->vy = particles[j]->vy;
-      */
     }
         
     /* Update particles */
     for (short i = 0; i < N; i++) {
-      update(particles[i], i, G, particlesPrev, N, epsilon, delta_t);
+      update((particles[i]), i, G, particlesPrev, N, epsilon, delta_t);
     }
     
     /* Do graphics. */
@@ -105,11 +105,11 @@ int main(int argc, char *argv[]) {
   /* Write file */
   double outdata[N*5];
   for (int i = 0; i < N; i++) {
-    outdata[5*(i) + 0] = particles[i]->x;
-    outdata[5*(i) + 1] = particles[i]->y;
-    outdata[5*(i) + 2] = particles[i]->m;
-    outdata[5*(i) + 3] = particles[i]->vx;
-    outdata[5*(i) + 4] = particles[i]->vy;
+    outdata[5*i] = particles[i]->x;
+    outdata[5*i + 1] = particles[i]->y;
+    outdata[5*i + 2] = particles[i]->m;
+    outdata[5*i + 3] = particles[i]->vx;
+    outdata[5*i + 4] = particles[i]->vy;
   }
   flag = write_doubles_to_file(N*5, outdata, "result.gal");
   printf("Writing output file: flag = %i\n", flag);
@@ -118,7 +118,7 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < N; i++) {
     free(particles[i]);
     free(particlesPrev[i]);
-  }
+  } //*/
   
   return 0;
 }
