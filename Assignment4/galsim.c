c8160f27f6fecc749dba3f15568f900212f82246
mbertolino <mattias.bertolino@gmail.com>
2017-02-14 16:11:22 +0100

    Insert method

--- Assignment4/galsim.c
+++ Assignment4/galsim.c
@@ -19,11 +19,98 @@
 
 struct quad_node {
 	struct quad_node *nw, *ne, *sw, *se;
-	double vx, vy;
+	double origo_x, origo_y;
+	double width;
 	double x, y;
-	double mass;
-	int level;
+	double m;
+	double vx, vy;
+	int size;
 } node_t;
+
+/* Insert a new node */
+void insert(node_t **node, double origo_x, double origo_y, double width,
+						double x, double y, double m, double vx, double vy) {
+	
+	/* If null, this is a leaf node */
+	if ((*node) == NULL) {
+		*node = (node_t*)malloc(sizeof(node_t));
+		(*node)->origo_x = origo_x;
+		(*node)->origo_y = origo_y;
+		(*node)->width = width;
+		(*node)->x = x;
+		(*node)->y = y;
+		(*node)->m = m;
+		(*node)->vx = vx;
+		(*node)->vy = vy;
+		(*node)->size = 1;
+		(*node)->nw = NULL;
+		(*node)->ne = NULL;
+		(*node)->sw = NULL;
+		(*node)->se = NULL;
+	
+	/* If this is a leaf node, split the node */
+	} else if (size == 1) {
+		(*node)->size += 1;
+		
+		/* Add previous particle */
+		if ((*node)->x < (*node)->origo_x) {
+			if ((*node)->y < (*node)->origo_y) // South West
+				insert((&(*node)->sw), (*node)->x, (*node)->y,
+							 (*node)->m, (*node)->vx, (*node)->vy, width/2);
+			else // North West
+				insert((&(*node)->nw), (*node)->x, (*node)->y,
+							 (*node)->m, (*node)->vx, (*node)->vy, width/2);
+		} else {
+			if ((*node)->y < (*node)->origo_y) // South East
+				insert((&(*node)->se), (*node)->x, (*node)->y,
+							 (*node)->m, (*node)->vx, (*node)->vy, width/2);
+			else // North East
+				insert((&(*node)->ne), (*node)->x, (*node)->y,
+							 (*node)->m, (*node)->vx, (*node)->vy, width/2);
+		}
+		
+		/* Add new particle */
+		if (x < (*node)->origo_x) {
+			if (y < (*node)->origo_y) // South West
+				insert((&(*node)->sw), x, y, m, vx, vy, width/2);
+			else // North West
+				insert((&(*node)->nw), x, y, m, vx, vy, width/2);
+		} else {
+			if (y < (*node)->origo_y) // South East
+				insert((&(*node)->se), x, y, m, vx, vy, width/2);
+			else // North East
+				insert((&(*node)->ne), x, y, m, vx, vy, width/2);
+		}
+		
+		/* Update this nodes properties */
+		(*node)->x = ((*node)->x + x)/2;
+		(*node)->y = ((*node)->y + y)/2;
+		(*node)->m += m;
+		//(*node)->vx = 0;
+		//(*node)->vy = 0;
+		
+	/* If this is a cluster node */
+	} else {
+		
+		/* Add new particle */
+		if (x < (*node)->origo_x) {
+			if (y < (*node)->origo_y) // South West
+				insert((&(*node)->sw), x, y, m, vx, vy, width/2);
+			else // North West
+				insert((&(*node)->nw), x, y, m, vx, vy, width/2);
+		} else {
+			if (y < (*node)->origo_y) // South East
+				insert((&(*node)->se), x, y, m, vx, vy, width/2);
+			else // North East&(*node)->ne), x, y, m, vx, vy, width/2);
+		}
+		
+		/* Update this nodes properties */
+		(*node)->size += 1;
+		(*node)->x = (((*node)->x)*size + x)/(size + 1);
+		(*node)->y = (((*node)->y)*size + y)/(size + 1);
+		(*node)->m += m;
+	}
+} //*/
 
 int main(int argc, char *argv[]) {
   printf("Size of struct: %lu\n", sizeof(struct quad_node));
